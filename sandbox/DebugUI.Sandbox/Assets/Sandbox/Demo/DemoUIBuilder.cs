3f2a846d3096440837305b39423d0b03ea316c65
AnnulusGames <annulusgames@gmail.com>
2024-02-03 14:02:03 +0900

    Add: demo scene

---
+++ sandbox/DebugUI.Sandbox/Assets/Sandbox/Demo/DemoUIBuilder.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DebugUI.Sandbox
+{
+    public class DemoUIBuilder : DebugUIBuilderBase
+    {
+        static DemoUIBuilder instance;
+
+        [SerializeField] GameObject prefab;
+
+        protected override void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(instance);
+            base.Awake();
+        }
+
+        protected override void Configure(IDebugUIBuilder builder)
+        {
+            builder.ConfigureWindowOptions(options =>
+            {
+                options.WindowName = "Demo";
+            });
+
+            builder.AddLabel("Physics");
+            builder.AddButton("Add Circle", () => Instantiate(prefab));
+            builder.AddSlider("Time Scale", 0f, 2f, () => Time.timeScale, x => Time.timeScale = x);
+
+            builder.AddLabel("Scene");
+            builder.AddButton("Reload", () => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
+        }
+    }
+}
