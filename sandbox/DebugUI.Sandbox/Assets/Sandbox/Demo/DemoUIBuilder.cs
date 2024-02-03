f197446f39bd972e92cfdaa13bc9af9eec1a0773
AnnulusGames <annulusgames@gmail.com>
2024-02-03 14:48:48 +0900

    Update: demo project

--- sandbox/DebugUI.Sandbox/Assets/Sandbox/Demo/DemoUIBuilder.cs
+++ sandbox/DebugUI.Sandbox/Assets/Sandbox/Demo/DemoUIBuilder.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 namespace DebugUI.Sandbox
@@ -8,6 +10,23 @@ namespace DebugUI.Sandbox
         static DemoUIBuilder instance;
 
         [SerializeField] GameObject prefab;
+        [SerializeField] Volume volume;
+
+        ColorAdjustments colorAdjustments;
+        Bloom bloom;
+
+        float GravityScale
+        {
+            get
+            {
+                return Physics2D.gravity.y / -9.81f;
+            }
+            set
+            {
+                var x = Physics2D.gravity.x;
+                Physics2D.gravity = new Vector2(x, value * -9.81f);
+            }
+        }
 
         protected override void Awake()
         {
@@ -19,6 +38,10 @@ namespace DebugUI.Sandbox
 
             instance = this;
             DontDestroyOnLoad(instance);
+
+            volume.profile.TryGet(out colorAdjustments);
+            volume.profile.TryGet(out bloom);
+
             base.Awake();
         }
 
@@ -30,11 +53,14 @@ namespace DebugUI.Sandbox
             });
 
             builder.AddLabel("Physics");
+            builder.AddSlider("Time Scale", 0f, 3f, () => Time.timeScale, x => Time.timeScale = x);
+            builder.AddSlider("Gravity Scale", 0f, 3f, () => GravityScale, x => GravityScale = x);
             builder.AddButton("Add Circle", () => Instantiate(prefab));
-            builder.AddSlider("Time Scale", 0f, 2f, () => Time.timeScale, x => Time.timeScale = x);
+            builder.AddButton("Reset", () => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
 
-            builder.AddLabel("Scene");
-            builder.AddButton("Reload", () => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
+            builder.AddLabel("Post-processing");
+            builder.AddSlider("Hue Shift", -180f, 180f, () => colorAdjustments.hueShift.value, x => colorAdjustments.hueShift.value = x);
+            builder.AddSlider("Bloom Intensity", 0f, 10f, () => bloom.intensity.value, x => bloom.intensity.value = x);
         }
     }
 }
